5358f0e26563d4e0f8bc4e8e073d54ba72ad9a3d
Travis Raybold <travis@raybold.com>
2021-05-24 08:41:59 -0700

    start script - get id from args

---
+++ test.ps1
@@ -0,0 +1,18 @@
+# get directory id from args
+$directoryId=$args[0]
+write-host $directoryId
+
+
+# get list of images in directory
+
+
+# determine number of pages
+
+#loop through pages
+
+  # create montage for page
+
+
+#print all montages
+
+#clean up montages
