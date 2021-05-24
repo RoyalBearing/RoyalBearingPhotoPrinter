9caf6479a9281578d31aa483ee09bf9adc674e9a
Travis Raybold <travis@raybold.com>
2021-05-24 08:55:03 -0700

    Get list of files in directory

--- test.ps1
+++ test.ps1
@@ -1,10 +1,15 @@
+# config
+$rootDir = "Z:\"
+
+
 # get directory id from args
 $directoryId=$args[0]
 write-host $directoryId
 
 
 # get list of images in directory
-
+$orderDir = "$rootDir$directoryId\"
+Get-ChildItem $orderDir
 
 # determine number of pages
 
