d823135d353d486312f60eb290d6d06769998830
Travis Raybold <travis@raybold.com>
2021-05-25 13:55:07 -0700

    Clean up messages, include page number in tmp image file name so it doesnt override

--- process.ps1
+++ process.ps1
@@ -17,32 +17,31 @@ function Print-RBPhotos {
     $count = $images.Count
     $numPages = [Math]::Ceiling($count/$maxPerPage)
     Write-Host "Found: $count images"
-    Write-Host "We need: $numPages pages"
+#    Write-Host "Processing: $numPages pages"
 
     Set-Location -Path $orderDir
     #loop through pages
     for ($i=0; $i -lt $numPages; $i++) {
         $pageStart = $i*$maxPerPage
         $pageEnd = ($i+1)*$maxPerPage-1
-        Write-Host "Handling page: $i, [$pageStart - $pageEnd]"
+        $currentPage = $i+1
+        Write-Host "Processing page: $currentPage of $numPages"
         $imagesForPage = $images[$pageStart..$pageEnd]
         $imageList = ""
         foreach ($image in $imagesForPage) {
             $imageList = "$imageList $image"
         }
-        Write-Host $imageList
         # create montage for page
-        Write-Host "magick  montage  $imageList montage22.png"
-        $MagickParameters += 'montage'
-        $MagickParameters += " ( $imageList ) "
-        $MagickParameters += 'montage22.png'
-        #    &'magick' $MagickParameters
-        $tmpImage = "C:\Git\PhotoPrinter\tmp\$directoryId.jpg"
-        $command = "magick montage -tile $tile -size $resize -frame 5  -geometry +5+5   $imageList  $tmpImage"
+        $tmpImage = "C:\Git\PhotoPrinter\tmp\$directoryId_$i.jpg"
+        $command = "@magick montage -tile $tile -size $resize -frame 5  -geometry +5+5   $imageList  $tmpImage"
         $tmpFile = "C:\\Git\\PhotoPrinter\\tmp\\$directoryId.bat"
         Set-Content -Path $tmpFile -Value $command
         Start-Process -NoNewWindow -Wait $tmpFile
+        Write-Host "Printing..."
         Start-Process  C:\windows\system32\mspaint.exe -Arg "/p $tmpImage /pt TravisOffice"
         #    Start-Process -NoNewWindow -FilePath magick.exe -ArgumentList 'montage --% -tile $tile -frame 5  -geometry +4+4 ( $imageList ) montage.png"
     }
+    Write-Host "Complete"
+    Write-Host ""
+    Write-Host ""
 }
